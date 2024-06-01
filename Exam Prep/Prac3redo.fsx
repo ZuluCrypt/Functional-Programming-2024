61b23a59259d224f0076000cac98adbdcd413e27
Zulu crypt <g20z6722@campus.ru.ac.za>
2024-06-01 02:08:50 +0200

    created some auxillarry and high-o func

--- Exam Prep/Prac3redo.fsx
+++ Exam Prep/Prac3redo.fsx
@@ -1,1 +1,42 @@
 // in this practical i will be looking at higher order functions and how to use them
+
+type myList<'a> =
+    |Empty
+    |Elements of 'a * myList<'a>
+//******************Auxillary functions********************************
+let push v l = Elements(v,l)//push
+let rec length list =
+    match list with
+    |[] -> 0
+    |_::rest -> 1 + length rest 
+    //length for built in list
+let rec length2 list =
+    match list with
+    |Empty -> 0
+    |Elements(_,nested) -> 1 + length2 nested
+    //length for generic list
+let reverse list=
+    let rec rev output l =
+        match l with
+        |Empty -> output
+        |Elements(a,rest)-> rev (push a output) rest
+    rev Empty list
+//********************************************************************************
+//List related Higher order functions
+//list.Map
+let rec B_listMap func =
+    function
+    |[] -> []
+    |a::rest -> func a :: B_listMap func rest
+
+B_listMap (fun x -> x + 2) [2;5;3;2;5]
+
+//list map for generic list
+let genListMap func list=
+    let rec map output l =
+        match l with
+        |Empty -> reverse(output)
+        |Elements(a,rest) -> map (push (func a) output) rest
+    map Empty list
+
+genListMap (fun x -> x + 2) (Elements(2,Elements(3,Empty)))
